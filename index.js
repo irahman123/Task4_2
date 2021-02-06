d39ce33b6cc7276733e3629b2fe80b02c3837fce
irahman123 <irahman23@yahoo.com>
2021-02-06 03:30:56 +0100

    first express server created

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+app=express();
+
+app.get("/", function (req, res){
+    res.send("Hello Express js");
+});
+
+app.listen(8000, function (){
+    console.log("server success")
+})
